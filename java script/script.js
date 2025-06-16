ac009149cb0c3356c02eba9be9eb514a6fa5044f
madhumitha <madhumithakrishna.06@gmail.com>
2025-06-16 15:38:37 +0530

    commit message

---
+++ java script/script.js
@@ -0,0 +1,33 @@
+var a="18";
+var b="20";
+console.log(a==b);
+console.log(a==b);
+console.log(al=b);
+console.log(al=b);
+var num =2;
+var result=num%2===0?"even":"odd"
+console.log(result)
+var mark=34;
+var result=(mark>=80)?"o":(mark>=60)?"A":(mark>=35)?"p"
+console.log(result)
+
+console.log(a*b)
+
+
+console.log(typeof a);
+var c;
+console.log(typeof c);
+var c=null;
+console.log(typeof c);
+var c=null;
+console.log(typeof c);
+
+
+
+
+
+
+
+
+
+
